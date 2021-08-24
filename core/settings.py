24736783772aa6a448c44ec6dfea9df079dbba01
johnspeny <johnspeny@gmail.com>
2021-08-25 00:24:59 +0300

    Email setup

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 from django.conf import settings
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -179,3 +182,13 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+
+# Email setup
+''' GMAIL SETUP '''
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['E_H_P']
+EMAIL_HOST_USER = os.environ['E_H_U']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- person/views.py
+++ person/views.py
@@ -1,24 +1,76 @@
 from .serializers import RegisterGreenUserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework import viewsets
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from django.template.loader import render_to_string
+
+
+
+from . models import GreenUser
 # Create your views here.
 
 
-class GreenUserCreate(APIView):
+class GreenUserCreate(generics.GenericAPIView):
 
-    permission_classes = [AllowAny]
+    serializer_class = RegisterGreenUserSerializer
 
     def post(self, request):
-        reg_serializer = RegisterGreenUserSerializer(data=request.data)
-        if reg_serializer.is_valid():
-            newuser = reg_serializer.save()
-            if newuser:
-                return Response(status=status.HTTP_201_CREATED)
-        return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # save user in db
+        reg_serializer = self.serializer_class(data=request.data)
+        reg_serializer.is_valid(raise_exception=True)
+        reg_serializer.save()
+
+        # get serialized data
+        user_data = reg_serializer.data
+
+        # get user from db
+        user =  GreenUser.objects.get(email=user_data['email'])
+
+        # email verification setup
+        # 1. get access token for user
+        acc_token = RefreshToken.for_user(user).access_token
+
+        # 2. get current domain
+        current_site =  get_current_site(request).domain
+
+        # 3. reverse email
+        # relativeLink = reverse('email-verify')
+
+        # absurl = 'http://'+ current_site+relativeLink+"?token="+str(acc_token)
+        
+        # email_body = 'Hi '+ user.user_name + \
+        #     ' Use the link below to verify your email \n' + absurl
+        
+    
+
+        # send mail
+        message = render_to_string('person/acc_active_email.html', {
+            'username': user.user_name,
+            'user': user.email,
+            'domain': current_site,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': str(acc_token)
+        })
+        send_mail('Verification Message',
+                    message,
+                    'okkerjohn@gmail.com',
+                    [str(user.email)],
+                    fail_silently=False, html_message=message)
+
+
+        user_data = reg_serializer.data
+        # if reg_serializer.is_valid():
+        #     newuser = reg_serializer.save()
+        #     if newuser:
+        #         return Response(status=status.HTTP_201_CREATED)
+        return Response(user_data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BlacklistTokenView(APIView):
