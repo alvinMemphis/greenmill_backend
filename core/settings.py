46c1ece055a024ca062c5aaed11e17285212eafd
johnspeny <johnspeny@gmail.com>
2021-08-24 17:25:31 +0300

    Actors created

--- core/settings.py
+++ core/settings.py
@@ -130,6 +130,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Custom user model
+AUTH_USER_MODEL = "person.GreenUser"
+
 # allowed origins eg from React Frontend
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000"
--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from person.models import GreenUser
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea, CharField
+from django import forms
+from django.db import models
 
-# Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    model = GreenUser
+    search_fields = ('email', 'user_name',)
+    list_filter = ('email', 'user_name', 'start_date', 'is_active', 'is_staff')
+    readonly_fields = ('id', 'start_date')
+    ordering = ('-start_date',)
+    list_display = ('id', 'email', 'user_name', 'start_date',
+                    'is_active', 'is_staff')
+
+    fieldsets = ()
+    # fieldsets = (
+    #     (None, {'fields': ('email', 'user_name',)}),
+    #     ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    #     ('Personal', {'fields': ('about',)}),
+    # )
+    # formfield_overrides = {
+    #     models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+    # }
+    # add_fieldsets = (
+    #     (None, {
+    #         'classes': ('wide',),
+    #         'fields': ('email', 'user_name', 'password1', 'password2', 'is_active', 'is_staff')}
+    #      ),
+    # )
+
+
+admin.site.register(GreenUser, UserAdminConfig)
